9ad5573ecca295bd07b11c5d773600a3949273b7
varunadithya99 <57499975+varunadithya99@users.noreply.github.com>
2020-04-07 12:43:20 +0530

    Update fibonnacci series.c

--- fibonnacci series.c
+++ fibonnacci series.c
@@ -1,24 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
-
+int a[10000]={0};
 void *fibonnacci(void *n);
-
 void main()
 {
 	pthread_t th1;
 	int n,i;
 	printf("\nEnter number of terms you want to print=");
 	scanf("%d",&n);
-	int *a;
 	while(n<=0)
 	{
 		printf("\nenter value bigger than 0!");
 		scanf("%d\n",&n);
 	}
 	pthread_create(&th1,NULL,fibonnacci,(void*)&n);
-	pthread_join(th1,(void*)&a);
-	a = *(int*)a;
+	pthread_join(th1,NULL);
 	for(i=0;i<n;i++)
 	{
 		printf("%d ",a[i]);
@@ -27,7 +24,7 @@ void main()
 void *fibonnacci(void *n)
 {
 	int f1=0,f2=1,x = *(int*)n,i;
-	int a[x],nxt;
+	int nxt;
 	for(i=0;i<x;i++)
 	{
 		a[i]=f1;
@@ -35,5 +32,4 @@ void *fibonnacci(void *n)
 		f1 = f2;
 		f2 = nxt;
 	}
-	pthread_exit(a);
 }
